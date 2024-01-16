d9189080bff0c47c1b9a80259258277b5eb362ee
Mohankumar <mohan2609kumar@gmail.com>
2024-01-16 17:33:09 +0530

    Viyaga WebView

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './screens/homeScreen/HomeScreen';
+import { StatusBar } from 'expo-status-bar';
+import WebviewScreen from './screens/webviewScreen/WebviewScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <StatusBar style='auto' />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title:"Viyaga WebView"}}/>
+          <Stack.Screen name="WebView" component={WebviewScreen} 
+          options={{ title: "Viyaga Software Solutions",  headerTitleAlign: 'center'}} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
